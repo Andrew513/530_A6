220ee8782f2b586e0c3a59e7c010a7550adc4b90
Andrew513 <brave.lei40915@gmail.com>
2024-11-11 05:08:10 -0600

    first commit, group_by in parserTypes need fix

---
+++ Main/SQL/headers/ExprTree.h
@@ -0,0 +1,680 @@
+
+#ifndef SQL_EXPRESSIONS
+#define SQL_EXPRESSIONS
+
+#include "MyDB_AttType.h"
+#include <string>
+#include <vector>
+#include "MyDB_Catalog.h"
+
+// create a smart pointer for database tables
+using namespace std;
+class ExprTree;
+typedef shared_ptr <ExprTree> ExprTreePtr;
+
+// this class encapsules a parsed SQL expression (such as "this.that > 34.5 AND 4 = 5")
+
+// class ExprTree is a pure virtual class... the various classes that implement it are below
+class ExprTree {
+
+public:
+	virtual string toString () = 0;
+	virtual string getType() = 0;
+	virtual ~ExprTree () {}
+	virtual bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) = 0;
+};
+
+class BoolLiteral : public ExprTree {
+
+private:
+	bool myVal;
+public:
+	
+	BoolLiteral (bool fromMe) {
+		myVal = fromMe;
+	}
+
+	string toString () {
+		if (myVal) {
+			return "bool[true]";
+		} else {
+			return "bool[false]";
+		}
+	}	
+	string getType() {
+		return "BOOL";
+	}
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		return true;
+	}
+};
+
+class DoubleLiteral : public ExprTree {
+
+private:
+	double myVal;
+public:
+
+	DoubleLiteral (double fromMe) {
+		myVal = fromMe;
+	}
+
+	string toString () {
+		return "double[" + to_string (myVal) + "]";
+	}	
+	string getType() {
+		return "Double";
+	}
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		return true;
+	}
+
+	~DoubleLiteral () {}
+};
+
+// this implement class ExprTree
+class IntLiteral : public ExprTree {
+
+private:
+	int myVal;
+public:
+
+	IntLiteral (int fromMe) {
+		myVal = fromMe;
+	}
+
+	string toString () {
+		return "int[" + to_string (myVal) + "]";
+	}
+
+	string getType() {
+		return "Int";
+	}
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		return true;
+	}
+
+	~IntLiteral () {}
+};
+
+class StringLiteral : public ExprTree {
+
+private:
+	string myVal;
+public:
+
+	StringLiteral (char *fromMe) {
+		fromMe[strlen (fromMe) - 1] = 0;
+		myVal = string (fromMe + 1);
+	}
+
+	string toString () {
+		return "string[" + myVal + "]";
+	}
+
+	string getType() {
+		return "STRING";
+	}
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		return true;
+	}
+
+	~StringLiteral () {}
+};
+
+// SELECT {employees.name, empoyees.salary} FROM employees WHERE employees.salary > 1000;
+class Identifier : public ExprTree {
+
+private:
+	string tableName;
+	string attName;
+	string attType;
+public:
+
+	Identifier (char *tableNameIn, char *attNameIn) {
+		tableName = string (tableNameIn);
+		attName = string (attNameIn);
+		attType = "IDENTIFIER";
+	}
+
+	// table name = employees;
+	// att name = name/salary;
+	string toString () {
+		return "[" + tableName + "_" + attName + "]";
+	}	
+
+	string getType() {
+		return attType;
+	}
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		bool found = false;
+		for(auto table : tablesToProcess) {
+			if(table.second == tableName) {
+				found = true;
+				break;
+			}
+		}
+		if(!found) {
+			cout << "Table " << tableName << " is not in the from clause.\n";
+			return false;
+		}
+		string attributeType;
+		bool attributesExist = catalog->getString(tableName+"."+attName+".type", attributeType);
+
+		if(!attributesExist) {
+			cout << "Attribute " << attName << " does not exist in table " << tableName << ".\n";
+			return false;
+		}
+
+		if(attributeType == "int" || attributeType == "double") {
+			attType = "NUMERIC";
+		} else if(attributeType == "string") {
+			attType = "STRING";
+		} else if(attributeType == "bool") {
+			attType = "BOOL";
+		}
+		
+		return true;
+	}
+
+	~Identifier () {}
+};
+
+class MinusOp : public ExprTree {
+
+private:
+
+	ExprTreePtr lhs;
+	ExprTreePtr rhs;
+	
+public:
+
+	MinusOp (ExprTreePtr lhsIn, ExprTreePtr rhsIn) {
+		lhs = lhsIn;
+		rhs = rhsIn;
+	}
+
+	string toString () {
+		return "- (" + lhs->toString () + ", " + rhs->toString () + ")";
+	}	
+
+	string getType() {
+		return "NUMERIC";
+	}
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		if(!lhs->semanticChecking(catalog, tablesToProcess) || !rhs->semanticChecking(catalog, tablesToProcess)) {
+			return false;
+		}
+		if(lhs->getType() != "NUMERIC" || rhs->getType() != "NUMERIC") {
+			cout << "Operands of - operator must be numeric.\n";
+			return false;
+		}
+		if (lhs->getType() != rhs->getType()) {
+			cout << "Operands of - operator must have the same type.\n";
+			return false;
+		}
+		return true;
+	}
+
+	~MinusOp () {}
+};
+
+class PlusOp : public ExprTree {
+
+private:
+
+	ExprTreePtr lhs;
+	ExprTreePtr rhs;
+	string attType;
+	
+public:
+
+	PlusOp (ExprTreePtr lhsIn, ExprTreePtr rhsIn) {
+		lhs = lhsIn;
+		rhs = rhsIn;
+		string attType = "NUMERIC";
+	}
+
+	string toString () {
+		return "+ (" + lhs->toString () + ", " + rhs->toString () + ")";
+	}	
+
+	string getType() {
+		return "NUMERIC";
+	}
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		if(!lhs->semanticChecking(catalog, tablesToProcess) || !rhs->semanticChecking(catalog, tablesToProcess)) {
+			return false;
+		}
+		if(lhs->getType() != rhs->getType()) {
+			cout << "Operands of + operator must have the same type.\n";
+			return false;
+		}
+		if(lhs->getType() == "STRING") {
+			attType = "STRING";
+			return true;
+		} else if(lhs->getType() == "NUMERIC") {
+			attType = "NUMERIC";
+			return true;
+		} else {
+			cout << "Operands of + operator must be numeric or string.\n";
+			return false;
+		}
+		return true;
+	}
+
+
+	~PlusOp () {}
+};
+
+class TimesOp : public ExprTree {
+
+private:
+
+	ExprTreePtr lhs;
+	ExprTreePtr rhs;
+	
+public:
+
+	TimesOp (ExprTreePtr lhsIn, ExprTreePtr rhsIn) {
+		lhs = lhsIn;
+		rhs = rhsIn;
+	}
+
+	string toString () {
+		return "* (" + lhs->toString () + ", " + rhs->toString () + ")";
+	}	
+
+	string getType() {
+		return "NUMERIC";
+	}
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		if(!lhs->semanticChecking(catalog, tablesToProcess) || !rhs->semanticChecking(catalog, tablesToProcess)) {
+			return false;
+		}
+		if(lhs->getType() != "NUMERIC" || rhs->getType() != "NUMERIC") {
+			cout << "Operands of * operator must be numeric.\n";
+			return false;
+		}
+		if(lhs->getType() != rhs->getType()) {
+			cout << "Operands of * operator must have the same type.\n";
+			return false;
+		}
+		return true;
+	}
+
+	~TimesOp () {}
+};
+
+class DivideOp : public ExprTree {
+
+private:
+
+	ExprTreePtr lhs;
+	ExprTreePtr rhs;
+	
+public:
+
+	DivideOp (ExprTreePtr lhsIn, ExprTreePtr rhsIn) {
+		lhs = lhsIn;
+		rhs = rhsIn;
+	}
+
+	string toString () {
+		return "/ (" + lhs->toString () + ", " + rhs->toString () + ")";
+	}	
+
+	string getType() {
+		return "NUMERIC";
+	}
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		if(!lhs->semanticChecking(catalog, tablesToProcess) || !rhs->semanticChecking(catalog, tablesToProcess)) {
+			return false;
+		}
+
+		if(rhs->toString() == "0") {
+			cout << "Division by zero.\n";
+			return false;
+		}
+
+		if(lhs->getType() != "NUMERIC" || rhs->getType() != "NUMERIC") {
+			cout << "Operands of / operator must be numeric.\n";
+			return false;
+		}
+		if(lhs->getType() != rhs->getType()) {
+			cout << "Operands of / operator must have the same type.\n";
+			return false;
+		}
+		return true;
+	}
+
+	~DivideOp () {}
+};
+
+class GtOp : public ExprTree {
+
+private:
+
+	ExprTreePtr lhs;
+	ExprTreePtr rhs;
+	
+public:
+
+	GtOp (ExprTreePtr lhsIn, ExprTreePtr rhsIn) {
+		lhs = lhsIn;
+		rhs = rhsIn;
+	}
+
+	string toString () {
+		return "> (" + lhs->toString () + ", " + rhs->toString () + ")";
+	}
+
+	string getType() {
+		return "BOOL";
+	}	
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		if(!lhs->semanticChecking(catalog, tablesToProcess) || !rhs->semanticChecking(catalog, tablesToProcess)) {
+			return false;
+		}
+		
+		if(lhs->getType() != rhs->getType()) {
+			cout << "Operands of > operator must have the same type.\n";
+			return false;
+		}
+
+		if(lhs->getType() != "NUMERIC" || rhs->getType() != "NUMERIC") {
+			cout << "Operands of > operator must be numeric.\n";
+			return false;
+		}
+		
+		return true;
+	}
+
+	~GtOp () {}
+};
+
+class LtOp : public ExprTree {
+
+private:
+
+	ExprTreePtr lhs;
+	ExprTreePtr rhs;
+	
+public:
+
+	LtOp (ExprTreePtr lhsIn, ExprTreePtr rhsIn) {
+		lhs = lhsIn;
+		rhs = rhsIn;
+	}
+
+	string toString () {
+		return "< (" + lhs->toString () + ", " + rhs->toString () + ")";
+	}
+
+	string getType() {
+		return "BOOL";
+	}
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		if(!lhs->semanticChecking(catalog, tablesToProcess) || !rhs->semanticChecking(catalog, tablesToProcess)) {
+			return false;
+		}
+		
+		if(lhs->getType() != rhs->getType()) {
+			cout << "Operands of < operator must have the same type.\n";
+			return false;
+		}
+
+		if(lhs->getType() != "NUMERIC" || rhs->getType() != "NUMERIC") {
+			cout << "Operands of < operator must be numeric.\n";
+			return false;
+		}
+		
+		return true;
+	}
+
+
+	~LtOp () {}
+};
+
+class NeqOp : public ExprTree {
+
+private:
+
+	ExprTreePtr lhs;
+	ExprTreePtr rhs;
+	
+public:
+
+	NeqOp (ExprTreePtr lhsIn, ExprTreePtr rhsIn) {
+		lhs = lhsIn;
+		rhs = rhsIn;
+	}
+
+	string toString () {
+		return "!= (" + lhs->toString () + ", " + rhs->toString () + ")";
+	}
+
+	string getType() {
+		return "BOOL";
+	}	
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		if(!lhs->semanticChecking(catalog, tablesToProcess) || !rhs->semanticChecking(catalog, tablesToProcess)) {
+			return false;
+		}
+		
+		if(lhs->getType() != rhs->getType()) {
+			cout << "Operands of != operator must have the same type.\n";
+			return false;
+		}
+
+		if(lhs->getType() != "NUMERIC" || rhs->getType() != "NUMERIC") {
+			cout << "Operands of != operator must be numeric.\n";
+			return false;
+		}
+		
+		return true;
+	}
+
+	~NeqOp () {}
+};
+
+class OrOp : public ExprTree {
+
+private:
+
+	ExprTreePtr lhs;
+	ExprTreePtr rhs;
+	
+public:
+
+	OrOp (ExprTreePtr lhsIn, ExprTreePtr rhsIn) {
+		lhs = lhsIn;
+		rhs = rhsIn;
+	}
+
+	string toString () {
+		return "|| (" + lhs->toString () + ", " + rhs->toString () + ")";
+	}
+
+	string getType() {
+		return "BOOL";
+	}
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		if(!lhs->semanticChecking(catalog, tablesToProcess) || !rhs->semanticChecking(catalog, tablesToProcess)) {
+			return false;
+		}
+
+		if(lhs->getType() != "BOOL" || rhs->getType() != "BOOL") {
+			cout << "Operands of || operator must be boolean.\n";
+			return false;
+		}
+
+		if(lhs->getType() != rhs->getType()) {
+			cout << "Operands of || operator must have the same type.\n";
+			return false;
+		}
+		
+		return true;
+	}	
+
+	~OrOp () {}
+};
+
+class EqOp : public ExprTree {
+
+private:
+
+	ExprTreePtr lhs;
+	ExprTreePtr rhs;
+	
+public:
+
+	EqOp (ExprTreePtr lhsIn, ExprTreePtr rhsIn) {
+		lhs = lhsIn;
+		rhs = rhsIn;
+	}
+
+	string toString () {
+		return "== (" + lhs->toString () + ", " + rhs->toString () + ")";
+	}	
+
+	string getType() {
+		return "BOOL";
+	}
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		if(!lhs->semanticChecking(catalog, tablesToProcess) || !rhs->semanticChecking(catalog, tablesToProcess)) {
+			return false;
+		}
+		
+		if(lhs->getType() != rhs->getType()) {
+			cout << "Operands of == operator must have the same type.\n";
+			return false;
+		}
+
+		if(lhs->getType() != "NUMERIC" && lhs->getType() != "STRING" && lhs->getType() != "BOOL") {
+			cout << "Operands of == operator must be numeric, string or boolean.\n";
+			return false;
+		}
+		
+		return true;
+	}
+
+	~EqOp () {}
+};
+
+class NotOp : public ExprTree {
+
+private:
+
+	ExprTreePtr child;
+	
+public:
+
+	NotOp (ExprTreePtr childIn) {
+		child = childIn;
+	}
+
+	string toString () {
+		return "!(" + child->toString () + ")";
+	}
+
+	string getType() {
+		return "BOOL";
+	}
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		if(!child->semanticChecking(catalog, tablesToProcess)) {
+			return false;
+		}
+		if(child->getType() != "BOOL") {
+			cout << "Operand of ! operator must be boolean.\n";
+			return false;
+		}
+		return true;
+	}	
+
+	~NotOp () {}
+};
+
+class SumOp : public ExprTree {
+
+private:
+
+	ExprTreePtr child;
+	
+public:
+
+	SumOp (ExprTreePtr childIn) {
+		child = childIn;
+	}
+
+	string toString () {
+		return "sum(" + child->toString () + ")";
+	}
+
+	string getType() {
+		return "NUMERIC";
+	}	
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		if(!child->semanticChecking(catalog, tablesToProcess)) {
+			return false;
+		}
+		if(child->getType() != "NUMERIC") {
+			cout << "Operand of sum operator must be numeric.\n";
+			return false;
+		}
+		return true;
+	}
+
+	~SumOp () {}
+};
+
+class AvgOp : public ExprTree {
+
+private:
+
+	ExprTreePtr child;
+	
+public:
+
+	AvgOp (ExprTreePtr childIn) {
+		child = childIn;
+	}
+
+	string toString () {
+		return "avg(" + child->toString () + ")";
+	}
+
+	string getType() {
+		return "NUMERIC";
+	}
+
+	bool semanticChecking(MyDB_CatalogPtr catalog, vector<pair<string, string>> tablesToProcess) {
+		if(!child->semanticChecking(catalog, tablesToProcess)) {
+			return false;
+		}
+		if(child->getType() != "NUMERIC") {
+			cout << "Operand of avg operator must be numeric.\n";
+			return false;
+		}
+		return true;
+	}	
+
+	~AvgOp () {}
+};
+
+#endif
---
+++ Main/SQL/headers/ParserTypes.h
@@ -0,0 +1,362 @@
+
+#ifndef PARSER_TYPES_H
+#define PARSER_TYPES_H
+
+#include <iostream>
+#include <stdlib.h>
+#include "ExprTree.h"
+#include "MyDB_Catalog.h"
+#include "MyDB_Schema.h"
+#include "MyDB_Table.h"
+#include <string>
+#include <utility>
+
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/*************************************************/
+/** HERE WE DEFINE ALL OF THE STRUCTS THAT ARE **/
+/** PASSED AROUND BY THE PARSER                **/
+/*************************************************/
+
+// structure that encapsulates a parsed computation that returns a value
+struct Value {
+
+private:
+
+        // this points to the expression tree that computes this value
+        ExprTreePtr myVal;
+
+public:
+        ~Value () {}
+
+        Value (ExprTreePtr useMe) {
+                myVal = useMe;
+        }
+
+        Value () {
+                myVal = nullptr;
+        }
+	
+	friend struct CNF;
+	friend struct ValueList;
+	friend struct SFWQuery;
+	#include "FriendDecls.h"
+};
+
+// structure that encapsulates a parsed CNF computation
+struct CNF {
+
+private:
+
+        // this points to the expression tree that computes this value
+        vector <ExprTreePtr> disjunctions;
+
+public:
+        ~CNF () {}
+
+        CNF (struct Value *useMe) {
+              	disjunctions.push_back (useMe->myVal); 
+        }
+
+        CNF () {}
+
+	friend struct SFWQuery;
+	#include "FriendDecls.h"
+};
+
+// structure that encapsulates a parsed list of value computations
+struct ValueList {
+
+private:
+
+        // this points to the expression tree that computes this value
+        vector <ExprTreePtr> valuesToCompute;
+
+public:
+        ~ValueList () {}
+
+        ValueList (struct Value *useMe) {
+              	valuesToCompute.push_back (useMe->myVal); 
+        }
+
+        ValueList () {}
+
+	friend struct SFWQuery;
+	#include "FriendDecls.h"
+};
+
+
+// structure to encapsulate a create table
+struct CreateTable {
+
+private:
+
+	// the name of the table to create
+	string tableName;
+
+	// the list of atts to create... the string is the att name
+	vector <pair <string, MyDB_AttTypePtr>> attsToCreate;
+
+	// true if we create a B+-Tree
+	bool isBPlusTree;
+
+	// the attribute to organize the B+-Tree on
+	string sortAtt;
+
+public:
+	string addToCatalog (string storageDir, MyDB_CatalogPtr addToMe) {
+
+		// make the schema
+		MyDB_SchemaPtr mySchema = make_shared <MyDB_Schema>();
+		for (auto a : attsToCreate) {
+			mySchema->appendAtt (a);
+		}
+
+		// now, make the table
+		MyDB_TablePtr myTable;
+
+		// just a regular file
+		if (!isBPlusTree) {
+			myTable =  make_shared <MyDB_Table> (tableName, 
+				storageDir + "/" + tableName + ".bin", mySchema);	
+
+		// creating a B+-Tree
+		} else {
+			
+			// make sure that we have the attribute
+			if (mySchema->getAttByName (sortAtt).first == -1) {
+				cout << "B+-Tree not created.\n";
+				return "nothing";
+			}
+			myTable =  make_shared <MyDB_Table> (tableName, 
+				storageDir + "/" + tableName + ".bin", mySchema, "bplustree", sortAtt);	
+		}
+
+		// and add to the catalog
+		myTable->putInCatalog (addToMe);
+
+		return tableName;
+	}
+
+	CreateTable () {}
+
+	CreateTable (string tableNameIn, vector <pair <string, MyDB_AttTypePtr>> atts) {
+		tableName = tableNameIn;
+		attsToCreate = atts;
+		isBPlusTree = false;
+	}
+
+	CreateTable (string tableNameIn, vector <pair <string, MyDB_AttTypePtr>> atts, string sortAttIn) {
+		tableName = tableNameIn;
+		attsToCreate = atts;
+		isBPlusTree = true;
+		sortAtt = sortAttIn;
+	}
+	
+	~CreateTable () {}
+
+	#include "FriendDecls.h"
+};
+
+// structure that stores a list of attributes
+struct AttList {
+
+private:
+
+	// the list of attributes
+	vector <pair <string, MyDB_AttTypePtr>> atts;
+
+public:
+	AttList (string attName, MyDB_AttTypePtr whichType) {
+		atts.push_back (make_pair (attName, whichType));
+	}
+
+	~AttList () {}
+
+	friend struct SFWQuery;
+	#include "FriendDecls.h"
+};
+
+struct FromList {
+
+private:
+
+	// the list of tables and aliases
+	vector <pair <string, string>> aliases;
+
+public:
+	FromList (string tableName, string aliasName) {
+		aliases.push_back (make_pair (tableName, aliasName));
+	}
+
+	FromList () {}
+
+	~FromList () {}
+	
+	friend struct SFWQuery;
+	#include "FriendDecls.h"
+};
+
+
+// structure that stores an entire SFW query
+struct SFWQuery {
+
+private:
+
+	// the various parts of the SQL query
+	vector <ExprTreePtr> valuesToSelect;
+	vector <pair <string, string>> tablesToProcess;
+	vector <ExprTreePtr> allDisjunctions;
+	vector <ExprTreePtr> groupingClauses;
+
+public:
+	SFWQuery () {}
+
+	SFWQuery (struct ValueList *selectClause, struct FromList *fromClause, 
+		struct CNF *cnf, struct ValueList *grouping) {
+		valuesToSelect = selectClause->valuesToCompute;
+		tablesToProcess = fromClause->aliases;
+		allDisjunctions = cnf->disjunctions;
+		groupingClauses = grouping->valuesToCompute;
+	}
+
+	SFWQuery (struct ValueList *selectClause, struct FromList *fromClause, 
+		struct CNF *cnf) {
+		valuesToSelect = selectClause->valuesToCompute;
+		tablesToProcess = fromClause->aliases;
+		allDisjunctions = cnf->disjunctions;
+	}
+
+	SFWQuery (struct ValueList *selectClause, struct FromList *fromClause) {
+		valuesToSelect = selectClause->valuesToCompute;
+		tablesToProcess = fromClause->aliases;
+		allDisjunctions.push_back (make_shared <BoolLiteral> (true));
+	}
+	
+	~SFWQuery () {}
+
+	void print () {
+		cout << "Selecting the following:\n";
+		for (auto a : valuesToSelect) {
+			cout << "\t" << a->toString () << "\n";
+		}
+		cout << "From the following:\n";
+		for (auto a : tablesToProcess) {
+			cout << "\t" << a.first << " AS " << a.second << "\n";
+		}
+		cout << "Where the following are true:\n";
+		for (auto a : allDisjunctions) {
+			cout << "\t" << a->toString () << "\n";
+		}
+		cout << "Group using:\n";
+		for (auto a : groupingClauses) {
+			cout << "\t" << a->toString () << "\n";
+		}
+	}
+
+	void semanticChecking (MyDB_CatalogPtr catalog) {
+		// Retrieve the list of tables from the catalog
+		vector<string> tableNames;
+		catalog->getStringList("tables", tableNames);
+		
+		// Check if all tables in the query exist in the catalog
+		for(const auto& table: tablesToProcess) {
+			bool tableExist = find(tableNames.begin(), tableNames.end(), table.first) != tableNames.end();
+			if(!tableExist) {
+				cout << "Table " << table.first << " does not exist in the catalog.\n";
+				return;
+			}
+		}
+
+		// find if the value is in the group by clause
+		for (const auto& valueToSelect : valuesToSelect) {
+			if (valueToSelect->getType() == "IDENTIFIER") {
+				bool isInGroupingClauses = std::any_of(
+					groupingClauses.begin(), 
+					groupingClauses.end(), 
+					[&](const auto& groupingClause) {
+						return groupingClause->getType() == "IDENTIFIER" && 
+							valueToSelect->toString() == groupingClause->toString();
+					}
+				);
+
+				if (!isInGroupingClauses) {
+					std::cout << "Error: Selected attribute " << valueToSelect->toString() 
+							<< " is not in grouping attributes." << std::endl;
+					return;
+				}
+			}
+		}
+
+		// Run checkQuery on individual query elements
+		// valuesToSelect:這個變數通常代表 SQL 查詢中的選擇欄位（即 SELECT 子句中的欄位）。例如，在 SELECT column1, column2 FROM table 中，valuesToSelect 會包含 column1 和 column2。
+		// allDisjunctions:這個變數通常代表 SQL 查詢中的所有不相交條件（即 WHERE 子句中的條件）。例如，在 WHERE condition1 OR condition2 中，allDisjunctions 會包含 condition1 和 condition2。
+		// groupingClauses:這個變數通常代表 SQL 查詢中的分組條件（即 GROUP BY 子句中的欄位）。例如，在 GROUP BY column1, column2 中，groupingClauses 會包含 column1 和 column2。
+		for(const auto& element: {valuesToSelect, allDisjunctions, groupingClauses}) {
+			for(const auto& item: element) {
+				if(!item->semanticChecking(catalog, tablesToProcess)) {
+					cout << "Column " << item->toString() << " is not in the group by clause.\n";
+					return;
+				}
+			}
+		}
+
+		cout<<"Query is semantically correct.\n";
+
+	}
+	#include "FriendDecls.h"
+};
+
+// structure that sores an entire SQL statement
+struct SQLStatement {
+
+private:
+
+	// in case we are a SFW query
+	SFWQuery myQuery;
+	bool isQuery;
+
+	// in case we a re a create table
+	CreateTable myTableToCreate;
+	bool isCreate;
+
+public:
+	SQLStatement (struct SFWQuery* useMe) {
+		myQuery = *useMe;
+		isQuery = true;
+		isCreate = false;
+	}
+
+	SQLStatement (struct CreateTable *useMe) {
+		myTableToCreate = *useMe;
+		isQuery = false;
+		isCreate = true;
+	}
+
+	bool isCreateTable () {
+		return isCreate;
+	}
+
+	bool isSFWQuery () {
+		return isQuery;
+	}
+
+	string addToCatalog (string storageDir, MyDB_CatalogPtr addToMe) {
+		return myTableToCreate.addToCatalog (storageDir, addToMe);
+	}		
+	
+	void printSFWQuery () {
+		myQuery.print ();
+	}
+
+	void semanticChecking (MyDB_CatalogPtr catalog) {
+		myQuery.semanticChecking(catalog);
+	}
+
+	#include "FriendDecls.h"
+};
+
+#endif
